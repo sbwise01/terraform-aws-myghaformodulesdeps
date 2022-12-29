d02eed6f579701c1999614638280cfa9fa3f648c
Brad Wise <>
2022-12-29 15:05:17 -0600

    Overcome env assignment issue in gha

--- .github/scripts/setup_terraform.sh
+++ .github/scripts/setup_terraform.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-cat > ${TF_CLI_CONFIG_FILE} <<EOL
+cat > ${GITHUB_WORKSPACE}/.terraformrc <<EOL
 credentials "app.terraform.io" {
   token = "${TF_TOKEN_app_terraform_io}"
 }
 EOL
+
+cat > $GITHUB_ENV <<EOL2
+export TF_CLI_CONFIG_FILE="${GITHUB_WORKSPACE}/.terraformrc"
+EOL2
