642bfc3430970f8b1af87325ffa4684887fe81a5
Brad Wise <>
2022-12-29 15:10:53 -0600

    Use home directory for terraformrc

--- .github/scripts/setup_terraform.sh
+++ .github/scripts/setup_terraform.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Github workspace is ${GITHUB_WORKSPACE} and env is ${GITHUB_ENV}"
-
-cat > ${GITHUB_WORKSPACE}/.terraformrc <<EOL
+cat > ~/.terraformrc<<EOL
 credentials "app.terraform.io" {
   token = "${TF_TOKEN_app_terraform_io}"
 }
 EOL
-
-cat > ${GITHUB_ENV} <<EOL2
-export TF_CLI_CONFIG_FILE="${GITHUB_WORKSPACE}/.terraformrc"
-EOL2
-
-echo "contents of ENV:"
-cat ${GITHUB_ENV}
